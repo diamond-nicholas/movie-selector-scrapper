749342452f1c6b54c7f8c70f0c909780fb0e80e5
diamond-nicholas <diamondnicholas154@gmail.com>
2022-06-21 19:49:53 +0100

    scrape movie years, actor_list, titles, ratings from imdb

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def main():
 
  soup = BeautifulSoup(html, 'html.parser')
  movietag = soup.select('td.titleColumn')
- movietag0 = movietag[0]
+ inner_movietags = soup.select('td.titleColumn a')
+ rating_tags = soup.select('td.posterColumn span[name=ir]')
 
  def get_year(movie_tag):
   moviesplit = movie_tag.text.split()
@@ -21,9 +22,10 @@ def main():
   return year
  
  years = [get_year(tag) for tag in movietag]
+ actors_list = [tag['title'] for tag in inner_movietags]
+ titles = [tag.text for tag in inner_movietags]
+ ratings = [float(tag['data-value']) for tag in rating_tags]
 
- # print(moviesplit)
- 
 
 
 
